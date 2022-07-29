b6a25085dcd19b864f68a588887643906e37b0a2
drummes12 <daesgodu@gmail.com>
2022-07-28 20:40:07 -0500

    Practice 1 - 2

--- test_1/script_test.js
+++ test_1/script_test.js
@@ -3,6 +3,7 @@
  * .parrafp {}
  * #pid {}
  */
+//  Lectura de los elementos de HTML
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafo = document.querySelector('.parrafo');
@@ -19,22 +20,27 @@ console.log({
     input
 });
 
+// Cambios en los elementos
 // h1.innerHTML = 'Hello,<br> world!';
 h1.innerText = 'Hello,<br> world!';
 // console.log(h1.getAttribute('class'));
 // h1.setAttribute('class', 'Hola Mundo');
 // console.log(h1.getAttribute('class'));
 
+// Consumo de las clases en HTML
 h1.classList.add('hola_mundo');
 h1.classList.remove('titulo');
 // h1.classList.toggle();
 // h1.classList.contains();
 
+// Cambios en los objetos de HTML
 input.value = 'drummes12';
 
+// Escritura HTML desde JS
 console.log(document.createElement('img'));
 const img = document.createElement('img');
 img.setAttribute('src', 'https://cnnespanol.cnn.com/wp-content/uploads/2022/07/220713165438-rba-web-nasa-full-169.jpg?quality=100&strip=info&w=414&h=311&crop=1');
 console.log(document.createElement('img'));
 
-pid.appendChild(img);
+// pid.innerHTML = '';
+pid.replaceWith(img);
---
+++ test_2/script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const result = document.querySelector("#result");
+
+const concatInputs = (event) => {
+    event.preventDefault();
+    const resultSum = input1.value + input2.value
+    result.innerText = `Resultado: ${resultSum}`;
+};
+
+form.addEventListener('submit', concatInputs);
+input2.addEventListener('keyup', concatInputs);
